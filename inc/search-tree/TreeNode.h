26f50b00e80ae3c39e5a76dd28dae3c74d1b93d3
Piggey <pepcio3320@gmail.com>
2022-07-06 18:52:16 +0200

    make it pretty

--- inc/search-tree/TreeNode.h
+++ inc/search-tree/TreeNode.h
@@ -9,13 +9,12 @@ namespace st
 struct TreeNode
 {
     // todo: make it work for utf-8 characters too
-    char c = -1;                            // single character
-    std::vector<TreeNode*> children;  // list of all children nodes
-    bool eow = false;                       // end of word flag
+    char c = -1;                        // single character
+    std::vector<TreeNode*> children;    // list of all children nodes
+    bool eow = false;                   // end of word flag
 
     TreeNode() = default;
 
-    // for std::vector.emplace_back()
     explicit TreeNode(char c)
     {
         this->c = c;
