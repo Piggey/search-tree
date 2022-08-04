5c99acffa8810e9793875293833562eb197ad3fa
Piggey <pepcio3320@gmail.com>
2022-08-04 14:22:39 +0200

    tests

--- test/tree_test.cpp
+++ test/tree_test.cpp
@@ -4,30 +4,20 @@
 
 #include <vector>
 #include <string>
-#include <queue>
 
 using st::Tree;
 using st::TreeNode;
-
-/**
- * @brief get the count of nodes currently used by the Tree
- * @param tree: Tree object pointer we want to check
- * @return count of the nodes (with root node!)
- */
-int get_tree_node_count(const Tree* tree);
 
 TEST(tree_class_test, constuctors)
 {
     // init empty Tree
     Tree t;
     EXPECT_EQ(t.size(), 0);
-    EXPECT_EQ(get_tree_node_count(&t), 1);
 
     // init Tree with words list
     Tree t2({ "widePeepoHappy", "widePeepoSad" });
 
     EXPECT_EQ(t2.size(), 2);
-    EXPECT_EQ(get_tree_node_count(&t2), 1 + 9 + 5 + 3);
 }
 
 TEST(tree_class_test, put)
@@ -35,16 +25,13 @@ TEST(tree_class_test, put)
     Tree t;
     t.put("Kappa");
     EXPECT_EQ(t.size(), 1);
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 5);
 
     t.put("Kapr");
     EXPECT_EQ(t.size(), 2);
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 5 + 1);
 
     // try to put the same word in
     t.put("Kapr");
     EXPECT_EQ(t.size(), 2);
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 5 + 1);
 }
 
 TEST(tree_class_test, remove)
@@ -59,8 +46,6 @@ TEST(tree_class_test, remove)
     t.put("kretyn");
     t.put("kreyoei");
 
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 6 + 4);
-
     // normal remove() use case
     EXPECT_EQ(t.find("kr").size(), 2);
 
@@ -69,24 +54,18 @@ TEST(tree_class_test, remove)
 
     EXPECT_EQ(t.find("kr")[0], "kreyoei");
 
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 6 + 4 - 3);
-
     // try to remove the same word
     t.remove("kretyn");
     EXPECT_EQ(t.find("kr").size(), 1);
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 6 + 4 - 3);
-
 
     // try to remove word that's nested inside another word
     // (don't delete any nodes, but decrement word counter)
     t.put("kretyn");
     t.put("kretynka");
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 6 + 4 + 2);
 
     EXPECT_EQ(t.size(), 3);
     t.remove("kretyn");
     EXPECT_EQ(t.size(), 2);
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 6 + 4 + 2);
 
     auto found = t.find("kr");
     EXPECT_EQ(found[0], "kreyoei");
@@ -96,7 +75,6 @@ TEST(tree_class_test, remove)
 TEST(tree_class_test, find)
 {
     Tree t({ "foo", "bar", "baz", "fizz", "buzz", "fizzbuzz" });
-    EXPECT_EQ(get_tree_node_count(&t), 1 + 3 + 3 + 1 + 3 + 3 + 4);
 
     // normal search
     std::vector<std::string> found = t.find("ba"); // bar, baz
@@ -127,24 +105,24 @@ TEST(tree_class_test, find)
     EXPECT_EQ(found.size(), 0);
 }
 
-int get_tree_node_count(const Tree* tree)
+TEST(tree_class_test, find_case_insensitive)
 {
-    int counter = 0;
-    std::queue<const TreeNode*> q;
+    Tree t({ "fiZZ", "fizzBUZZ", "fizzbuzz", "FIZZBUZZ", "[]square" });
 
-    q.push(tree->root());
-    counter++; // should i even consider a root node as a node?
+    auto found = t.find("fizzbuzz", true);
+    EXPECT_EQ(found.size(), 3);
+    EXPECT_EQ(found[0], "fizzbuzz");
+    EXPECT_EQ(found[1], "fizzBUZZ");
+    EXPECT_EQ(found[2], "FIZZBUZZ");
 
-    while (!q.empty())
-    {
-        const TreeNode* node = q.front(); q.pop();
+    // just checking whether it gets checked only on letters
+    found = t.find(";", true);
+    EXPECT_EQ(found.size(), 0);
 
-        for (const auto& child : node->children)
-        {
-            q.push(child);
-            counter++;
-        }
-    }
+    found = t.find("{", true);
+    EXPECT_EQ(found.size(), 0);
 
-    return counter;
+    // empty string as prefix
+    found = t.find("", true); // FIXME: these are NOT correct values! (not the problem with ignoring case sens i think)
+    EXPECT_EQ(found.size(), 5);
 }
