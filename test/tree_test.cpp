1cf17363e3f8c293320b1de95d1fb3b6957fac57
Piggey <pepcio3320@gmail.com>
2022-08-05 21:04:01 +0200

    add some new tests

--- test/tree_test.cpp
+++ test/tree_test.cpp
@@ -47,16 +47,16 @@ TEST(tree_class_test, remove)
     t.put("kreyoei");
 
     // normal remove() use case
-    EXPECT_EQ(t.find("kr").size(), 2);
+    EXPECT_EQ(t.size(), 2);
 
     t.remove("kretyn");
-    EXPECT_EQ(t.find("kr").size(), 1);
-
+    EXPECT_EQ(t.size(), 1);
     EXPECT_EQ(t.find("kr")[0], "kreyoei");
 
     // try to remove the same word
     t.remove("kretyn");
-    EXPECT_EQ(t.find("kr").size(), 1);
+    EXPECT_EQ(t.size(), 1);
+    EXPECT_EQ(t.find("kr")[0], "kreyoei");
 
     // try to remove word that's nested inside another word
     // (don't delete any nodes, but decrement word counter)
@@ -64,6 +64,7 @@ TEST(tree_class_test, remove)
     t.put("kretynka");
 
     EXPECT_EQ(t.size(), 3);
+
     t.remove("kretyn");
     EXPECT_EQ(t.size(), 2);
 
@@ -74,13 +75,15 @@ TEST(tree_class_test, remove)
 
 TEST(tree_class_test, find)
 {
-    Tree t({ "foo", "bar", "baz", "fizz", "buzz", "fizzbuzz" });
+    Tree t({ "foo", "bar", "bara", "bare", "baz", "fizz", "buzz", "fizzbuzz" });
 
     // normal search
-    std::vector<std::string> found = t.find("ba"); // bar, baz
-    EXPECT_EQ(found.size(), 2);
-    EXPECT_EQ(found[0], "bar");
-    EXPECT_EQ(found[1], "baz");
+    std::vector<std::string> found = t.find("b"); // bar, baz, bara, bare
+    std::vector<std::string> correct = { "bar", "bara", "bare", "baz" };
+
+    EXPECT_EQ(found.size(), 4);
+    for (unsigned int i = 0; i < found.size(); i++)
+        EXPECT_EQ(found[i], correct[i]);
 
     // prefix whole word
     found = t.find("fizzbuzz"); // fizzbuzz
@@ -93,7 +96,11 @@ TEST(tree_class_test, find)
 
     // search with empty string
     found = t.find("");
-    EXPECT_EQ(found.size(), 6);
+    EXPECT_EQ(found.size(), 8);
+
+    correct = { "foo", "fizz", "fizzbuzz", "bar", "bara", "bare", "baz", "buzz" };
+    for(unsigned int i = 0; i < correct.size(); i++)
+        EXPECT_EQ(found[i], correct[i]);
 
     // search something longer than existing word
     found = t.find("barz");
@@ -109,11 +116,16 @@ TEST(tree_class_test, find_case_insensitive)
 {
     Tree t({ "fiZZ", "fizzBUZZ", "fizzbuzz", "FIZZBUZZ", "[]square" });
 
-    auto found = t.find("fizzbuzz", true);
+    auto found = t.find("fizzb", true);
     EXPECT_EQ(found.size(), 3);
     EXPECT_EQ(found[0], "fizzbuzz");
     EXPECT_EQ(found[1], "fizzBUZZ");
     EXPECT_EQ(found[2], "FIZZBUZZ");
+
+    // 'FIZZBUZZ' first since its the most similar to the prefix
+    found = t.find("FIZZB", true);
+    EXPECT_EQ(found.size(), 3);
+    EXPECT_EQ(found[0], "FIZZBUZZ");
 
     // just checking whether it gets checked only on letters
     found = t.find(";", true);
@@ -123,6 +135,10 @@ TEST(tree_class_test, find_case_insensitive)
     EXPECT_EQ(found.size(), 0);
 
     // empty string as prefix
-    found = t.find("", true); // FIXME: these are NOT correct values! (not the problem with ignoring case sens i think)
+    found = t.find("", true);
+    std::vector<std::string> correct = { "fiZZ", "fizzBUZZ", "fizzbuzz", "FIZZBUZZ", "[]square" };
+
     EXPECT_EQ(found.size(), 5);
+    for (unsigned int i = 0; i < found.size(); i++)
+        EXPECT_EQ(found[i], correct[i]);
 }
